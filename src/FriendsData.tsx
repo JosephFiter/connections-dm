70cf495d5c22362f9efd46278222c44d70ccdb4a
Lucasszejn <roguenine6@gmail.com>
2025-04-05 09:51:58 -0300

    2

--- src/FriendsData.tsx
+++ src/FriendsData.tsx
@@ -1,12 +1,29 @@
 export const friends = [
-    { name: "Juan", category: "Tecnologia Ditela", image: "juan.png" },
-    { name: "Fefo", category: "No esta en Dibuje", image: "fefo.png" },
-    { name: "Jumbo", category: "Ex-ort", image: "jumbo.png" },
-    { name: "Mauri", category: "No esta en Dibuje", image: "mauri.png" },
-    { name: "Oliber", category: "Tecnologia Ditela", image: "oliber.png" },
-    { name: "Panter", category: "Tecnologia Ditela", image: "panter.png" },
-    { name: "Pava", category: "No esta en Dibuje", image: "pava.png" },
-    { name: "Raizman", category: "Ex-ort", image: "raizman.png" },
-    { name: "Lucacho", category: "Ex-ort", image: "lucacho.png" },
+  { name: "Uri", category: "Mica, la tana, la nueva, Eva Brown" },
+  { name: "Setton", category: "Mica, la tana, la nueva, Eva Brown" },
+  { name: "Felix", category: "No son de OnlyChamps" },
+  { name: "Vito", category: "No son de OnlyChamps" },
+  { name: "Naguito", category: "Mica, la tana, la nueva, Eva Brown" },
+  { name: "Suaya", category: "Vive en provincia" },
+  { name: "Castre", category: "Vive en provincia" },
+  { name: "Panter", category: "No son de OnlyChamps" },
+  { name: "Lucacho", category: "Vive en provincia" },
+].map(friend => ({
+  ...friend,
+  image: `${friend.name.toLowerCase()}.png`
+}));
+  /*
 
-  ];
+
+    { name: "Juan", category: "Tecnologia Ditela" },
+  { name: "Fefo", category: "No esta en Dibuje" },
+  { name: "Jumbo", category: "Ex-ort" },
+  { name: "Mauri", category: "No esta en Dibuje" },
+  { name: "Oliber", category: "Tecnologia Ditela" },
+  { name: "Panter", category: "Tecnologia Ditela" },
+  { name: "Pava", category: "No esta en Dibuje" },
+  { name: "Raizman", category: "Ex-ort" },
+  { name: "Lucacho", category: "Ex-ort" },
+
+
+  */
