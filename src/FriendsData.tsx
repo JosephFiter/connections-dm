2fbf1edfea2ebc7a80f1ae3466550d2b8eb85011
Lucasszejn <roguenine6@gmail.com>
2025-04-07 11:52:10 -0300

    dfg

--- src/FriendsData.tsx
+++ src/FriendsData.tsx
@@ -1,13 +1,13 @@
 export const friends = [
-  { name: "Panter", category: "Golf" },
-  { name: "Lucacho", category: "Celular android" },
-  { name: "Felix", category: "Futbol Hacoaj" },
-  { name: "Burstein", category: "Golf" },
-  { name: "Fefo", category: "Golf" },
-  { name: "Suaya", category: "Futbol Hacoaj" },
-  { name: "Chapu", category: "Celular android" },
-  { name: "Jumbo", category: "Celular android" },
-  { name: "Juan", category: "Futbol Hacoaj" },
+  { name: "Suaya", category: "Foto de perfil haciendo deporte" },
+  { name: "Pava", category: "Reconocidos por su apodo" },
+  { name: "Felix", category: "Foto de perfil haciendo deporte" },
+  { name: "Castre", category: "Reconocidos por su apodo" },
+  { name: "Fefo", category: "Federico" },
+  { name: "Saban", category: "Federico" },
+  { name: "Panter", category: "Reconocidos por su apodo" },
+  { name: "Raizman", category: "Federico" },
+  { name: "Ian", category: "Foto de perfil haciendo deporte" },
 ].map(friend => ({
   ...friend,
   image: `${friend.name.toLowerCase()}.png`
